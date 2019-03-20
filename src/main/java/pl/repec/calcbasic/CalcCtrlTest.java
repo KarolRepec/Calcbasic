187962c1c45dc668b47ece251cfd9773ce4df652
KarolRepec <karol.repec@gmail.com>
2019-03-20 12:56:37 +0100

    Added multiply, subtract and divide. Tuned ConsoleView methods.

--- src/main/java/pl/repec/calcbasic/CalcCtrlTest.java
+++ src/main/java/pl/repec/calcbasic/CalcCtrlTest.java
@@ -1,12 +1,21 @@
 package pl.repec.calcbasic;
 
 
-//FIXME: Add tests.
+import org.junit.Assert;
+import org.junit.Test;
+
 public class CalcCtrlTest {
 
+    @Test
     public void shouldPass() {
+
         CalcCtrl sut = new CalcCtrl();
-        System.out.println(sut);
+        System.out.println("test addition");
+
+        double result = sut.getResult(3.0,5.0, OperationType.ADDITION);
+
+        Assert.assertEquals(5, result, 0.0);
     }
+
 
 }
